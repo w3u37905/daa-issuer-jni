9b792a60b805f2af781bb27b1a760f932e979417
Menelaos Giannopoulos <mgiannopoulos@ubitech.eu>
2022-06-15 14:14:18 +0300

    Register issuer PK

--- jni-runner/src/main/java/jni/Runner.java
+++ jni-runner/src/main/java/jni/Runner.java
@@ -4,5 +4,27 @@ public class Runner {
     public static void main(String[] args) {
         Tools tools = new Tools();
         System.out.println(tools.foo() + tools.bar());
+
+        DAAInterface daa = new DAAInterface();
+
+        String ik = "-----BEGIN PUBLIC KEY-----\n" +
+                "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE4CwPPzL9DS6n2zcDsV1hOadgL25Q\n" +
+                "hTF3PuomKkE3/ET4GcPMTkYi8zd2IIUVI/FwY+sWTyHhCxrHkfXKksSAmA==\n" +
+                "-----END PUBLIC KEY-----";
+
+        String ik_priv = "-----BEGIN EC PRIVATE KEY-----\n" +
+                "MHcCAQEEIF8Cx/viWSyi0gCp/OcbMFJrbKmzO2PwlqA/RNtv9UMZoAoGCCqGSM49\n" +
+                "AwEHoUQDQgAE4CwPPzL9DS6n2zcDsV1hOadgL25QhTF3PuomKkE3/ET4GcPMTkYi\n" +
+                "8zd2IIUVI/FwY+sWTyHhCxrHkfXKksSAmA==\n" +
+                "-----END EC PRIVATE KEY-----\n";
+
+
+        byte[] issuerPk = ik.getBytes();
+        byte[] issuerPriv = ik_priv.getBytes();
+
+        daa.registerIssuerPK(issuerPk);
+        daa.registerIssuer_priv(issuerPriv);
+
+
     }
 }
